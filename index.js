ba614dd413af53880113955f3bec16fbadb32df9
Phill Barron <phillipbarron242@gmail.com>
2021-05-31 17:57:53 +0100

    removed test added index

---
+++ index.js
@@ -0,0 +1,19 @@
+var notp = require("notp").totp;
+const base32 = require("thirty-two");
+const clipboardy = require("clipboardy");
+
+const generateAuthCode = () => {
+  return notp.gen(base32.decode(process.env.E2E_TEST_ONE_TIME_PASSWORD_KEY));
+};
+
+const copyToClipboard = async (code) => {
+  await clipboardy.writeSync(code);
+};
+
+const go = async () => {
+  const code = generateAuthCode();
+  console.log(`auth code: ${code}`);
+  await copyToClipboard(code);
+};
+
+go();
--- test.js
+++
@@ -1,4 +0,0 @@
-var notp = require('notp').totp;
-const base32 = require('thirty-two');
-var code = notp.gen(base32.decode(process.env.E2E_TEST_ONE_TIME_PASSWORD_KEY));
-console.log(code)
