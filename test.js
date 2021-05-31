da0d547e025d705b2320bb406684dc5081dc3834
Phill Barron <phillipbarron242@gmail.com>
2021-05-31 17:57:14 +0100

    copying to clipboard for login without phone

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var notp = require('notp').totp;
 const base32 = require('thirty-two');
-var code = notp.gen(base32.decode("YOUR_ONE_TIME_PASWORD_TOKEN"));
-console.log(code) 
+var code = notp.gen(base32.decode(process.env.E2E_TEST_ONE_TIME_PASSWORD_KEY));
+console.log(code)
