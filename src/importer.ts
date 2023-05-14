8e3b80321dad71bb014cfc5a6163963242095679
Phill Barron <phillipbarron242@gmail.com>
2023-05-14 13:02:16 +0100

    adding some backup options to import

--- src/importer.ts
+++ src/importer.ts
@@ -1,4 +1,4 @@
-import { existsSync, readFileSync, writeFileSync } from 'fs';
+import { existsSync, readFileSync, writeFileSync, copyFileSync } from 'fs';
 import inquirer from 'inquirer';
 
 export interface ConfigItem {
@@ -57,10 +57,51 @@ const migrate = async () => {
     key: item.totpSecret,
   }));
 
+const configAlreadyExists = await hasExistingConfig();
+
+  if (configAlreadyExists) {
+    const EXIT = 'exit';
+    const REPLACE = 'replace existing config';
+    const BACKUP_AND_REPLACE = 'backup and replace existing config';
+    console.log("there is existing config - importing would overwrite");
+    const { action } = await inquirer.prompt(
+      [
+        {
+          type: 'rawlist',
+          name: 'action',
+          message: 'Which is better?',
+          choices: [EXIT, REPLACE, BACKUP_AND_REPLACE],
+        },
+      ]
+    )
+    switch (action) {
+      case EXIT:
+        console.log('bye!');
+        return;
+      case REPLACE: {
+        writeFileSync(
+          `${CONFIG_FILE_PATH}`,
+          JSON.stringify({ keys: asConfig }, null, 2),
+        );
+        console.log('file replaced');
+        return;
+      }
+      case BACKUP_AND_REPLACE: {
+        copyFileSync(CONFIG_FILE_PATH, `${CONFIG_FILE_PATH}.bak`)
+        console.log(`Copied config file to ${CONFIG_FILE_PATH}.bak`)
+        writeFileSync(
+          `${CONFIG_FILE_PATH}`,
+          JSON.stringify({ keys: asConfig }, null, 2),
+        );
+        return;
+      }
+    }
+  }
   writeFileSync(
     `${CONFIG_FILE_PATH}.new`,
     JSON.stringify({ keys: asConfig }, null, 2),
   );
+  console.log('config file created');
 };
 
 migrate();
